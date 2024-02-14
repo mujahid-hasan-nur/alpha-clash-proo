09b0f476491b8cfb41929c4b01f9cf76930cd497
mujahid-hasan-nur <mujahidhasannur@gmail.com>
2024-02-15 00:38:35 +0600

    add new

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -14,6 +14,14 @@ function continueGame(){
     //* step -1: generate a random alphabet //
     const alphabet = getARandomAlphabet();
     console.log('your random alphabet', alphabet);
+
+    //* set randomly generated alphabet to the screen (sho it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    //* set background color //
+
+    setBackgroundColorById(alphabet);
 }
 
 function play(){
