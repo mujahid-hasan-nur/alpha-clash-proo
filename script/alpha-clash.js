c1f405aad5a2aa8a64324f65cb75fe096288bc2e
mujahid-hasan-nur <mujahidhasannur@gmail.com>
2024-02-17 19:13:45 +0600

    add nothing

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -10,6 +10,27 @@
 //     // console.log(playgroundSection.classList)
 // }
 
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key;
+    console.log('player pressed', playerPressed);
+
+    //* get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, currentAlphabet);
+
+    //* check matched or not 
+    if(playerPressed === expectedAlphabet){
+        console.log('you get a point');
+    }
+    else{
+        console.log('you missed. you lost a life');
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent)
+
 function continueGame(){
     //* step -1: generate a random alphabet //
     const alphabet = getARandomAlphabet();
