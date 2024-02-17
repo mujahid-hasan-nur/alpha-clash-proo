26230753cfc3fcf744f664a674425f8986cc12b8
mujahid-hasan-nur <mujahidhasannur@gmail.com>
2024-02-17 23:12:57 +0600

    adddddd

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -12,29 +12,45 @@
 
 function handleKeyboardKeyUpEvent(event){
     const playerPressed = event.key;
-    console.log('player pressed', playerPressed);
+    // console.log('player pressed', playerPressed);
 
-    //* get the expected to press
+    //* key player is expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLowerCase();
-    console.log(playerPressed, currentAlphabet);
 
-    //* check matched or not 
-    if(playerPressed === expectedAlphabet){
-        console.log('you get a point');
+    //^^ check right or wrong key pressed 
+    if (playerPressed === expectedAlphabet) {
+        console.log('you got a point!');
+        console.log('you have a pressed correctly', expectedAlphabet);
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
     }
     else{
-        console.log('you missed. you lost a life');
+        console.log('dhurr vaiya ba apu..right key press koro')
     }
+
+    // // get the expected to press
+    // const currentAlphabetElement = document.getElementById('current-alphabet');
+    // const currentAlphabet = currentAlphabetElement.innerText;
+    // const expectedAlphabet = currentAlphabet.toLowerCase();
+    // console.log(playerPressed, expectedAlphabet);
+
+    // // check matched or not 
+    // if(playerPressed === expectedAlphabet){
+    //     console.log('you get a point');
+    // }
+    // else{
+    //     console.log('you missed. you lost a life');
+    // }
 }
 
-document.addEventListener('keyup', handleKeyboardKeyUpEvent)
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
 
 function continueGame(){
     //* step -1: generate a random alphabet //
     const alphabet = getARandomAlphabet();
-    console.log('your random alphabet', alphabet);
+    // console.log('your random alphabet', alphabet);
 
     //* set randomly generated alphabet to the screen (sho it)
     const currentAlphabetElement = document.getElementById('current-alphabet');
